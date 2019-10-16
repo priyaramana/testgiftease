e8a181a9d2d35a0745659347015525ce9f7ebab4
priya <tenapallipriya@gmail.com>
2019-10-16 15:47:26 +0600

    commit

--- src/free/Free.java
+++ src/free/Free.java
@@ -10,7 +10,8 @@ public class Free {
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\priya\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.facebook.com");
+		driver.get("https://www.giftease.com");
+		driver.manage().window().maximize();
 
 	}
 
